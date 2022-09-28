19c3627ccab6ee462b3dfdfbbb0bdb3a5f51ef07
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-09-28 18:09:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,84 @@
-// Get user's geolocation  
- var lat,  
-  lon,  
-  tWeather,  
-  icon,  
-  celsius,  
-  farenheit,  
-  iconNumber,  
-  minCelsius,  
-  maxCelsius,  
-  minF,  
-  maxF;  
- $.getJSON('https://ipapi.co/json/', function(data){  
-  console.log(data);  
-  // reaching the location API  
- console.log(data);  
-   // Finding the latitude and longitude, to be able to find the location  
-   $.each(data, function(k, v) {});  
-   lat = data.latitude;  
-   lon = data.longitude;  
-   $("#latitude").append(lat);  
-   $("#longitude").append(lon);  
-   $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=059dcee9c15c93a942eb1f38b72876be", function(weatherData) {  
-    $.each(weatherData, function(j, b) {});  
-    console.log(weatherData);  
-    // fetch image and display weather and icon down below   
-    tWeather = weatherData.main.temp;  
-    iconNumber = weatherData.weather[0].icon;  
-    icon = ("http://openweathermap.org/img/w/" + iconNumber + ".png");  
-    $("#graphic").append('<img src="' + icon + '"/>');  
-    $('#description').append(weatherData.weather[0].description);  
-    $('#city').append(weatherData.name);  
-    $("#windSpeed").append(weatherData.wind.speed + "km/h");  
-    $("#pressure").append(weatherData.main.pressure + "º");  
-    $("#humidity").append(weatherData.main.humidity + "%");  
-    minF = Math.floor(weatherData.main.temp_min * 9/5 - 459.67);  
-    maxF = Math.floor(weatherData.main.temp_max * 9/5 - 459.67);  
-    // translate Kelvin to Celsius  
-    celsius = Math.floor(weatherData.main.temp - 273.15);  
-    minCelsius = Math.floor(weatherData.main.temp_min - 273.15);  
-    maxCelsius = Math.floor(weatherData.main.temp_max - 273.15);  
-    // translate Kelvin to Farenhait T(K) × 9/5 - 459.67  
-    farenheit = Math.floor(weatherData.main.temp * 9 / 5 - 459.67);  
-    // display celsius by default   
-    $('#celsius').append(celsius + 'ºC');  
-    $("#min").append( minCelsius + 'ºC');  
-    $("#max").append( maxCelsius + 'ºC');  
-   });  
-  // background code here   
+const form = document.getElementById("form");  
+ const username = document.getElementById("username");  
+ const email = document.getElementById("email");  
+ const password = document.getElementById("password");  
+ const cPassword = document.getElementById("c-password");  
+
+ //Show Error Message  
+
+ function showError(input, message) {  
+  const formControl = input.parentElement;  
+  formControl.className = "input error";  
+  const small = formControl.querySelector("small");  
+  small.innerText = message;  
+ }  
+
+ //Show Success message  
+
+ function showSuccess(input) {  
+  const formControl = input.parentElement;  
+  formControl.classList.add("success");  
+ }  
+
+ //Check Required fields  
+
+ function checkRequired(inputArr) {  
+  inputArr.forEach(function (input) {  
+   if (input.value.trim() === "") {  
+    showError(input, `${getFieldName(input)} is required`);  
+   } else {  
+    showSuccess(input);  
+   }  
+  });  
+ }  
+
+ // Get Field Name  
+
+ function getFieldName(input) {  
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);  
+ }  
+
+ // Check Input Lenght  
+
+ function checkLenghth(input, min, max) {  
+  if (input.value.length < min) {  
+   showError(  
+    input,  
+    `${getFieldName(input)} must be at least ${min} characters `  
+   );  
+  } else if (input.value.length > max) {  
+   showError(  
+    input,  
+    `${getFieldName(input)} must be less than ${max} characters `  
+   );  
+  } else {  
+   showSuccess(input);  
+  }  
+ }  
+
+ // Check E-mail Is Valid  
+
+ function checkEmail(input) {  
+  const re = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;  
+  if (re.test(input.value.trim())) {  
+   showSuccess(input);  
+  } else {  
+   showError(input, "E-mail is not Valid");  
+  }  
+ }  
+
+ // Check Password Match  
+
+ function checkPasswordMatch(input1, input2) {  
+  if (input1.value !== input2.value) {  
+   showError(input2, "Passwords do not match");  
+  }  
+ }  
+ form.addEventListener("submit", (e) => {  
+  e.preventDefault();  
+  checkRequired([username, email, password, cPassword]);  
+  checkLenghth(username, 3, 15);  
+  checkLenghth(password, 8, 25);  
+  checkEmail(email);  
+  checkPasswordMatch(password, cPassword);  
  });  
- var switchButton = document.querySelector('.switch-button');  
- var switchBtnRight = document.querySelector('.switch-button-case.right');  
- var switchBtnLeft = document.querySelector('.switch-button-case.left');  
- var activeSwitch = document.querySelector('.active');  
- function switchLeft() {  
-  switchBtnRight.classList.remove('active-case');  
-  switchBtnLeft.classList.add('active-case');  
-  activeSwitch.style.left = '0%';  
-  $('#farenheit').empty();  
-  $('#min').empty();  
-  $('#max').empty();  
-  $('#celsius').append(celsius + 'ºC');  
-  $("#min").append(minCelsius + 'ºC');  
-  $("#max").append(maxCelsius + 'ºC');  
- }  
- function switchRight() {  
-  switchBtnRight.classList.add('active-case');  
-  switchBtnLeft.classList.remove('active-case');  
-  activeSwitch.style.left = '50%';  
-  $('#celsius').empty();  
-  $('#min').empty();  
-  $('#max').empty();  
-  $('#farenheit').append(farenheit + 'ºF');  
-  $('#min').append(minF + "ºF");  
-  $('#max').append(maxF + "ºF");  
- }  
- switchBtnLeft.addEventListener('click', function() {  
-  $('#celsius').empty();  
-  switchLeft();  
- }, false);  
- switchBtnRight.addEventListener('click', function() {  
-  $('#farenheit').empty();  
-  switchRight();  
- }, false);  
